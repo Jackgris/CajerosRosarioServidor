0c0cf16fbc4840d17342af1d39d06e0e83b5ceb3
jackgris <jackgris2@gmail.com>
2013-09-10 20:23:12 -0300

    mirando algunas caracteristicas

--- cajerorosario/cajerorosario.go
+++ cajerorosario/cajerorosario.go
@@ -1,6 +1,8 @@
 package cajerorosario
 
 import (
+	"appengine"
+    "appengine/user"
     "fmt"
     "net/http"
 )
@@ -10,5 +12,17 @@ func init() {
 }
 
 func cajerorosario(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Test App Engine</h1>")
+	c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "Bienvenido, %v!", u)
 }
