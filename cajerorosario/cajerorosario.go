86f1b99405c430910fd64486e7d55a87d8c8d1ed
jackgris <jackgris2@gmail.com>
2013-09-17 19:55:50 -0300

    probando cargar un html de diferentes archivos

--- cajerorosario/cajerorosario.go
+++ cajerorosario/cajerorosario.go
@@ -9,54 +9,37 @@ import (
     "time"
 )
 
-type Greeting struct {
+type Cajero struct {
     Author  string
     Content string
     Date    time.Time
 }
 
+var guestbookTemplate = template.Must(template.ParseFiles("templates/base.html", 
+	"templates/list.html"))
+
 func init() {
     http.HandleFunc("/", root)
     http.HandleFunc("/sign", sign)
 }
-
+ 
 func root(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
-    greetings := make([]Greeting, 0, 10)
+    greetings := make([]Cajero, 0, 10)
     if _, err := q.GetAll(c, &greetings); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    if err := guestbookTemplate.Execute(w, greetings); err != nil {
+    
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
 
-var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
-
-const guestbookTemplateHTML = `
-<html>
-  <body>
-    {{range .}}
-      {{with .Author}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Content}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`
-
 func sign(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
-    g := Greeting{
+    g := Cajero{
 		Content: r.FormValue("content"),
 		Date:    time.Now(),
     }
